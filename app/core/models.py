ac8e82159f828c6b3a28544d2f528a925ec23adc
redwanchowdhury <redwanc12@gmail.com>
2019-03-07 20:26:59 -0700

    fixed code format errors

--- app/core/models.py
+++ app/core/models.py
@@ -1,16 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+    PermissionsMixin
 
 
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create and saves a new user"""
-        if(not email):
+        if not email:
             raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
-        user.save(using = self._db)
+        user.save(using=self._db)
 
         return user
 
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
@@ -9,8 +10,8 @@ class ModelTests(TestCase):
         email = 'redwanc12@gmail.com'
         password = 'testPas'
         user = get_user_model().objects.create_user(
-        email = email,
-        password = password
+            email=email,
+            password=password
         )
 
         self.assertEqual(user.email, email)
@@ -33,7 +34,7 @@ class ModelTests(TestCase):
     def test_create_new_superuser(self):
         """test creating a new superuser"""
 
-        user= get_user_model().objects.create_superuser(
+        user = get_user_model().objects.create_superuser(
             'test@gmail.com',
             'test123'
         )
